2be873103bdf0276368ddb92d7dad0f4d32448d1
Evgeny Karpenko <johnny_headinair@mail.ru>
2018-02-23 12:31:32 +0700

    only author can edit his answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,12 +1,17 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_answer, only: [:destroy]
+  before_action :set_answer, only: [:update, :destroy]
   before_action :set_question, only: [:create]
 
   def create
     @answer = current_user.answers.build(answer_params)
     @answer.question = @question
     @answer.save
+  end
+
+  def update
+    @answer.update(answer_params)
+    @question = @answer.question
   end
 
   def destroy
