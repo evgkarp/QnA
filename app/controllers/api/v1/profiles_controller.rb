1019fc78c9bf4e46f928b578fd3ba32686f84658
Evgeny Karpenko <johnny_headinair@mail.ru>
2018-08-19 22:33:58 +0700

    add questions api

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,9 +1,5 @@
-class Api::V1::ProfilesController < ApplicationController
+class Api::V1::ProfilesController < Api::V1::BaseController
   authorize_resource class: User
-
-  before_action :doorkeeper_authorize!
-
-  respond_to :json
 
   def me
     respond_with current_resource_owner
@@ -11,15 +7,5 @@ class Api::V1::ProfilesController < ApplicationController
 
   def index
     respond_with User.where.not(id: current_resource_owner.id)
-  end
-
-  protected
-
-  def current_ability
-    @ability ||= Ability.new(current_resource_owner)
-  end
-
-  def current_resource_owner
-    @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
 end
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe 'Profile API' do
   describe 'GET /me' do
     context 'unauthorized' do
-      it 'returns 403 status if there is no access_token' do
+      it 'returns 401 status if there is no access_token' do
         get '/api/v1/profiles/me', params: { format: :json }
-        expect(response.status).to eq 403
+        expect(response.status).to eq 401
       end
 
-      it 'returns 403 status if access_token is invalid' do
+      it 'returns 401 status if access_token is invalid' do
         get '/api/v1/profiles/me', params: { format: :json, access_token: '1234' }
-        expect(response.status).to eq 403
+        expect(response.status).to eq 401
       end
     end
 
