e409eeece0af3986e810e666f9d31a1607bb6bdd
Evgeny Karpenko <johnny_headinair@mail.ru>
2018-09-19 21:43:20 +0700

    config action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "qna_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: '51.15.102.143' }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.smtp_settings = { address: '51.15.102.143', port: 25 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
