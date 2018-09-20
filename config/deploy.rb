372a18b673002f665d4172ccb90050b0562079c6
Evgeny Karpenko <johnny_headinair@mail.ru>
2018-09-20 20:15:34 +0700

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # execute :touch, release_path.join('tmp/restart.txt')
-      invoke 'unicorn: restart'
+      invoke 'unicorn:restart'
     end
   end
 
